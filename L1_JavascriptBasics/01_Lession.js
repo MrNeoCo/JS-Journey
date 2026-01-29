fe781f23ededb262b7c06739e4eb1e5ebcc87a02
MrNeoCo <neocoecomdev@gmail.com>
2026-01-29 23:04:00 +0530

    Lession - 2 Number and Math | Math.round and Operator Presidence

---
+++ L1_JavascriptBasics/01_Lession.js
@@ -0,0 +1,13 @@
+alert('Good Morning');
+alert('Nitin');
+console.log(10 + 5);
+console.log(20 - 5);
+console.log(2 + 2 - 5);
+document.body.innerHTML = "Good Morning";
+document.body.innerHTML = "Nitin";
+
+console.log("The total cost for Tshirt cost 10$, Socks costs 8$ and Dinner Plates costs 20$ is " + (10 + 8 + 20) + "$");
+
+console.log("Total Account Balance is : " + (100 - 20 - 50 + 200) + "$");
+
+document.body.innerHTML = "";
