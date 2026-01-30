6e767833afa64ad182dd73955c324890a552ab80
MrNeoCo <neocoecomdev@gmail.com>
2026-01-30 22:12:49 +0530

    Lession 2 Solution with code and Questions

--- L2_NumbersMath/02_Lession.js
+++ L2_NumbersMath/02_Lession.js
@@ -1,6 +1,13 @@
-console.log(10 + 3*8 + 5);
-console.log((10 + 3*8 + 5)*3);
-console.log((1850+799)/100);
-console.log(((1850+799)/100)*0.1);
-console.log(Math.round((1850+799)/100)*0.1);
-console.log(Math.round((1850+799)/100)*0.2);
+console.log(10 + 3 * 8 + 5);
+console.log((10 + 3 * 8 + 5) * 3);
+console.log((1850 + 799) / 100);
+console.log(((1850 + 799) / 100) * 0.1);
+console.log(Math.round((1850 + 799) / 100) * 0.1);
+console.log(Math.round((1850 + 799) / 100) * 0.2);
+
+
+console.log((2095 + 799 + 1899) / 100);
+console.log((2095 + 799 + 1899 + 499) / 100);
+console.log(Math.round((2095 + 799 + 1899 + 499) * 0.1) / 100);
+console.log(Math.round((2095 + 799 + 1899 + 499) + (2095 + 799 + 1899 + 499) * 0.1) / 100);
+
