18d67e8067b53f4654519ef920d0c01f8cd41b55
MrNeoCo <neocoecomdev@gmail.com>
2026-01-30 18:46:44 +0530

    Lession - 3 Strings, Concatenation, 3 different ways to Create Strings, Template strings, Interpolation

---
+++ L2_NumbersMath/02_Lession.js
@@ -0,0 +1,6 @@
+console.log(10 + 3*8 + 5);
+console.log((10 + 3*8 + 5)*3);
+console.log((1850+799)/100);
+console.log(((1850+799)/100)*0.1);
+console.log(Math.round((1850+799)/100)*0.1);
+console.log(Math.round((1850+799)/100)*0.2);
---
+++ L3_String/03_Lession.js
@@ -0,0 +1,23 @@
+console.log('My Name is Nitin');
+console.log('Your Name is Neo');
+
+console.log('My Name is Nitin'+' and Your Name is Neo');
+
+console.log('The Total Cost is $'+ (5+3));
+console.log(`The Total Cost is $${5+3}`);
+
+// alert(`The Total Cost is $${5+3}`);
+
+console.log('The Total Cost $'+ (599 + 295)/100 );
+
+console.log(`The Total Cost $${(599+295)/100}`);
+
+// alert(`The Total Cost $${(599+295)/100}`);
+
+// alert(`The Total Cost $${(599+295)/100} \nThanks you!, Come Again`);
+
+console.log(`Items(${2+2}): $${((2095+799)/100)*2}`);
+console.log(`Shipping & Handaling: $${(499*2)/100}`);
+console.log(`Total before tax: $${(((2095+799)/100)*2) + (499*2)/100}`);
+
+console.log(`Estimated tax(10%): $${Math.round(((2095+799+499)*2*0.1))/100}`);
