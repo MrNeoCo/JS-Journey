47693ec3109ba97d65bf43fe13aac71ce95a59a5
MrNeoCo <neocomecomdev@gmail.com>
2026-02-20 19:07:06 +0530

    final todo project made done

---
+++ L11 - Arrays-and-Loops part-2/todo-list-final-verson/11-todo-list.js
@@ -0,0 +1,62 @@
+  let todoArray = [
+    {
+    name: 'walk up early',
+    dueDate: '2026-02-20'
+     }, {
+    name: 'sleep early',
+    dueDate: '2026-02-20'
+    }];
+
+    renderTodoList();
+
+  function renderTodoList(){
+    let todoListHTML = '';
+
+    for(let i=0; i<todoArray.length; i++){
+
+      const todoObject = todoArray[i];
+
+      // const name = todoObject.name;
+      // const dueDate = todoObject.dueDate;
+      const {name,dueDate} = todoObject;
+
+      todoListHTML += `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button 
+          onclick="
+          todoArray.splice(${i}, 1);
+          renderTodoList();
+        " class="delete-todo-button">Delete</button>
+        `;
+      }
+      let showTodo = document.querySelector('.js-todo-list');
+        showTodo.innerHTML = todoListHTML
+      // console.log(todoListHTML);
+  }
+
+  function addTodo(){
+    const inputElement = document.querySelector('.input-element');
+    const dueDateElement = document.querySelector('.js-due-data-element');
+    const errorElement = document.querySelector('.js-error-element');
+
+    const name  = inputElement.value;
+    const dueDate = dueDateElement.value;
+
+    if(name === ''){
+      errorElement.innerHTML = `must name your todo`;
+      return;
+    }else{
+      errorElement.innerHTML = '';
+    }
+
+    todoArray.push({
+      // name: name,
+      // dueDate: dueDate
+      name,
+      dueDate
+    });
+    // console.log(todoArray);
+    inputElement.value = '';
+    renderTodoList();
+  }
