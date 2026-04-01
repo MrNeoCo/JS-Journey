c2c1f6790b44ced4c09a04b3248f3bc511db3ef0
MrNeoCo <neocoecomdev@gmail.com>
2026-04-02 00:33:57 +0530

    insted of amazon.js file's products[array of Objects] we used actual prepared data from data/products.js *What New i learn from it That you can write 2 and more script tags and load more than one script files* -----> just keep the files in such order in which you want it to be executed like in here i want my product data to be loads first so my data/products.js file is on the top who's execution results scripts/amazon.js file runs smoothly because it get the products from the earliest execution

--- L13 - javascript-amazon-project/scripts/amazon.js
+++ L13 - javascript-amazon-project/scripts/amazon.js
@@ -1,37 +1,40 @@
 //!======Main idea of JavaScript======
 //----1. Save the data---- 
-const product = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090 
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating:{
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating:{
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799,
-}];
+//- since now we are using this products Array of Object from data/products.js file 
+// const products = [{
+//   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//   rating: {
+//     stars: 4.5,
+//     count: 87
+//   },
+//   priceCents: 1090 
+// }, {
+//   image: 'images/products/intermediate-composite-basketball.jpg',
+//   name: 'Intermediate Size Basketball',
+//   rating:{
+//     stars: 4,
+//     count: 127
+//   },
+//   priceCents: 2095
+// }, {
+//   image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//   name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//   rating:{
+//     stars: 4.5,
+//     count: 56
+//   },
+//   priceCents: 799,
+// }];
+//---------------------------------------------------------------------------------
 
 
 //----2. Generate the HTML----
 
 //* const html = products.forEach( you need to write a function in it);
 let productsHTML = '';
-product.forEach((product)=>{
+
+products.forEach((product)=>{
 // productsHTML = productHTML +  ` 
   productsHTML +=  `
 
